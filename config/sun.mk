f0bbd64ee6a9247e90c0871198d20c66f6a04106
Nirmal Abraham <c_nabrah@qti.qualcomm.com>
2025-11-11 12:30:19 +0530

    ARM: dts: msm: Update camera reset gpios
    Update camera reset gpios for emmc variant
    of Kera.
    
    GPIO RST0 - Changed from gpio123 to gpio8.
    GPIO RST1 - Changed from gpio124 to gpio9.
    GPIO RST2 - Changed from gpio125 to gpio11.
    GPIO RST3 - Changed from gpio126 to gpio100.
    
    CRs-Fixed: 4353512
    Change-Id: I19564c58ea50e31fe342d1a062c0ba5b9db276fe

--- config/sun.mk
+++ config/sun.mk
@@ -14,6 +14,7 @@ dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera-sensor-mtp.dtbo \
 dtbo-$(CONFIG_ARCH_KERA)  += kera-camera.dtbo
 dtbo-$(CONFIG_ARCH_KERA)  += kera-camera-sensor-mtp.dtbo \
 				kera-camera-sensor-cdp.dtbo \
+				kera-camera-sensor-cdp-emmc.dtbo \
 				kera-camera-sensor-qrd.dtbo \
 				kera-camera-sensor-rcm.dtbo \
 				kera-camera-sensor-idp.dtbo
